c7b2087c7cf73dc67ac35da956eb6eed0b7d3bd0
LuciMachuca <lmachuca3329@gmail.com>
2022-02-23 13:36:54 -0300

    reorganizacion galeria

--- src/componentes/Galeria/Galeria.jsx
+++ src/componentes/Galeria/Galeria.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import Footer from '../Footer';
 //import Navbar from './HomeConfig';
 import Navbar from '../NavBar';
-import Parque from './GaleriaConfig.jsx';
+import Parque from './parqueController.jsx';
+//import Un_Dormitorio from './unDormController.jsx';
+//import Dos_Dormitorios from './dosDormController.jsx';
+
 
 
 export default function Galeria() {
