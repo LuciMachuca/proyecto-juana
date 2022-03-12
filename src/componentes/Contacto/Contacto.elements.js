43d3dba543431564fb031a97ce94296e3d3a061b
LuciMachuca <lmachuca3329@gmail.com>
2022-03-12 12:46:13 -0300

    footer y contactos

---
+++ src/componentes/Contacto/Contacto.elements.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+max-width: 1200px;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    position: relative;
+    overflow: hidden;
+    padding-top: 30px;
+    padding-left: 30px;
+    
+
+`;
+
+export const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    
+    
+
+`;
+
+export const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`;
+
+export const Image = styled.img`
+    height: 80%;
+
+`;
+
+export const InfoContainer = styled.div`
+    flex: 1;
+    padding-top: 120px;
+    padding-left: 40px;
+    margin:  0 auto;
+
+    
+`;
+
+export const Dir = styled.div`
+    display: flex;
+    padding-bottom: 15px;
+   line-height: 23px;
+    
+    
+`;
+
+export const Telefono = styled.div`
+    display: flex;
+    padding-bottom: 15px;
+    line-height: 25px;
+`;
+
+export const Email = styled.div`
+    display: flex;
+    
+`;
+
+export const Desc = styled.div`
+    font-size: 15px;
+    font-family: 'Spartan';
+    padding-left: 15px;
+    
+`;
+
+export const DescT = styled.div`
+    font-size: 15px;
+    font-family: 'Spartan';
+    padding-left: 15px;
+    padding-top: 3px;
+    
+`;
--- src/componentes/footer.jsx
+++ src/componentes/footer.jsx
@@ -5,6 +5,7 @@ import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import theme from '../themeConfig'
 import Logo from '../Imagenes/Logo_Limpio_m.png';
+import LogoBoo from '../Imagenes/booking_logo_icon.png';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
@@ -87,6 +88,20 @@ function Footer() {
                       
                             <Button target={'_blank'} href="mailto:juanacomplejo@hotmail.com" style={{color: 'black'}} >
                                 <EmailIcon />
+                            </Button>
+
+                            <Button target={'_blank'} href="https://www.booking.com/hotel/ar/complejo-de-cabanas-dona-juana.es-ar.html?aid=318615;label=Spanish_Argentina_ES_AR_20153587225-6h15TH8LvM25bNckrW9zZwS217247257083%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atidsa-162435869305%3Alp1000126%3Ali%3Adec%3Adm;sid=fcb97bf773f0806fba24ba07bf04ebfb;dest_id=-1005603;dest_type=city;dist=0;group_adults=2;group_children=0;hapos=1;hpos=1;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1646874831;srpvid=800b08a6554100d3;type=total;ucfs=1&#hotelTmpl" style={{color: 'black', height: '32px'}} >
+                                <Box component="img" margin={'auto'}
+                            
+                                sx={{
+                                width: '50%',    
+                                height: '97%',
+                                
+                                /* maxHeight: { xs: '80%', sm: '100%' },
+                                maxWidth: { xs: 500, sm: '100%' }, */
+                                }}
+                                alt="LogoB."
+                                src={LogoBoo} />
                             </Button>
                              
                              
