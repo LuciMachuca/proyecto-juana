bcf477f14b3ce8983c83d7970ee6ce4539211854
LuciMachuca <lmachuca3329@gmail.com>
2022-05-02 19:37:24 -0300

    equip

--- src/componentes/Home/Footer.jsx
+++ src/componentes/Home/Footer.jsx
@@ -71,7 +71,7 @@ function Footer() {
                             </Button>
 
                             <Button target={'_blank'} href="https://www.booking.com/hotel/ar/complejo-de-cabanas-dona-juana.es-ar.html?aid=318615;label=Spanish_Argentina_ES_AR_20153587225-6h15TH8LvM25bNckrW9zZwS217247257083%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atidsa-162435869305%3Alp1000126%3Ali%3Adec%3Adm;sid=fcb97bf773f0806fba24ba07bf04ebfb;dest_id=-1005603;dest_type=city;dist=0;group_adults=2;group_children=0;hapos=1;hpos=1;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1646874831;srpvid=800b08a6554100d3;type=total;ucfs=1&#hotelTmpl" style={{ color: 'black', height: '32px' }} >
-                                <Box component="img" margin={'auto'}
+                                <Box component="img" margin={'auto'} 
 
                                     sx={{
                                         width: '2rem',
